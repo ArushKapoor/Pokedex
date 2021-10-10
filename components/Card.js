3229d8b9854b729fbe3e2d778e946874779c7a68
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-10-10 19:04:11 +0530

    added tailwindcss

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
 import classes from "./Cards.module.css";
 
 function Card() {
-  return <div className={classes.blank}>Hello World</div>;
+  return <div className="text-blue-600">Hello World</div>;
 }
 
 export default Card;
